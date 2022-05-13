4798b0a3921aa3349e05c1c2c5422d888118dbad
Ahmed Roshdy <65695097+Ahmed-Roshdy-1@users.noreply.github.com>
2022-05-13 05:25:53 +0200

    Time: 138 ms (11.31%), Space: 47 MB (21.72%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (root == null) return root;
+    let queue = [root];
+    let level = [];
+    
+    while(queue.length) {
+        const node = queue.shift();
+        node.next = queue[0] || null;
+        if (node.left != null) { level.push(node.left) ;} 
+        if (node.right != null) { level.push(node.right) ;} 
+
+        if(queue.length == 0){         
+            queue = level;
+            level = [];
+        }
+     }
+    return root;
+    
+};
