5fbaeb7370b0b001ebe9f4cec915a8762befaf47
Ahmed Roshdy <65695097+Ahmed-Roshdy-1@users.noreply.github.com>
2022-07-26 15:34:33 +0200

    Time: 131 ms (28.31%), Space: 44.5 MB (48.40%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -3,21 +3,23 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-    nums = sortArr(nums);
+    nums.sort(function(a, b) {
+  return a - b;
+});
     for (let i = 0; nums.length; i++) {
         if (i != nums[i]) return i;
     }
     return nums.length;
 };
-var sortArr = function (n) {
-    for (let i = 0; i < n.length; i++) {
-        for (let j = 0; j < n.length - i; j++) {
-            if (n[j] > n[j + 1]) {
-                let temp = n[j];
-                n[j] = n[j + 1];
-                n[j + 1] = temp;
-            }
-        }
-    }
-    return n;
-};
+// var sortArr = function (n) {
+//     for (let i = 0; i < n.length; i++) {
+//         for (let j = 0; j < n.length - i; j++) {
+//             if (n[j] > n[j + 1]) {
+//                 let temp = n[j];
+//                 n[j] = n[j + 1];
+//                 n[j + 1] = temp;
+//             }
+//         }
+//     }
+//     return n;
+// };
